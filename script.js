6359cc18e38940f5c702cbd7356905d53923ccd2
sirtzn <richready1@gmail.com>
2026-02-04 01:03:00 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById("btn").addEventListener("click", () => {
-  alert("Button clicked!");
+const card = document.getElementById("card");
+
+document.addEventListener("mousemove", (e) => {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left - rect.width / 2;
+  const y = e.clientY - rect.top - rect.height / 2;
+
+  const rotateX = (-y / rect.height) * 15;
+  const rotateY = (x / rect.width) * 15;
+
+  card.style.transform = `
+    rotateX(${rotateX}deg)
+    rotateY(${rotateY}deg)
+  `;
+});
+
+document.addEventListener("mouseleave", () => {
+  card.style.transform = "rotateX(0deg) rotateY(0deg)";
 });
